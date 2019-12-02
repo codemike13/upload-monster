8681d85df9306df49f39c1746eea63fa894af4b9
Anthony Michael Stephens <mike.stephens@codeimmersives.com>
2019-12-02 00:48:24 -0500

    Made current with clas repo

---
+++ todo-backend/routes/todo/models/Todo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TodoSchema = new Schema({
+    todo: { type: String, default: '' },
+    user_id: { type: Schema.Types.ObjectId, ref: 'user' },
+    completed: { type: Boolean, default: false }
+})
+
+module.exports = mongoose.model(('todo', TodoSchema))
